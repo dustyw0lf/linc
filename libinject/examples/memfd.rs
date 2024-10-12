fd9cb8ec8319b3133b4254bc8803ab462b2d706b
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-12 22:50:16 +0300

    Refactor examples

--- libinject/examples/memfd.rs
+++ libinject/examples/memfd.rs
@@ -4,8 +4,8 @@ use libinject::memfd;
 use std::fs;
 
 fn main() {
-    // let binary_location = "/usr/bin/ls";
-    let binary_location = "http://127.0.0.1:8081/ls";
+    let binary_location = "/usr/bin/ls";
+    // let binary_location = "http://127.0.0.1:8081/ls";
     let binary_name = binary_location.split('/').last().unwrap();
     // let binary_bytes = get_binary_filesystem(binary_location);
     let binary_bytes = get_binary_http(binary_location);
