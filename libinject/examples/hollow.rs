66cf6af9853e128d0ba9e20ecaaf7ea6c0979b75
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-12 22:43:34 +0300

    Refactor examples

--- libinject/examples/hollow.rs
+++ libinject/examples/hollow.rs
@@ -1,18 +1,9 @@
-#![allow(unused_imports, unused_variables)]
+#![allow(unused_variables, dead_code)]
 extern crate libinject;
 use libinject::hollow;
 
 use std::env::current_dir;
 use std::fs;
-
-pub fn get_binary_filesystem(path: &str) -> Vec<u8> {
-    fs::read(path).expect("Faild to open file")
-}
-
-pub fn get_binary_http(url: &str) -> Vec<u8> {
-    let response = reqwest::blocking::get(url).expect("Failed to download file");
-    response.bytes().unwrap().to_vec()
-}
 
 fn main() {
     let path = current_dir().unwrap();
@@ -31,4 +22,13 @@ fn main() {
         Ok(res) => res,
         Err(error) => panic!("An error occured: {error:?}"),
     };
+}
+
+fn get_binary_filesystem(path: &str) -> Vec<u8> {
+    fs::read(path).expect("Faild to open file")
+}
+
+fn get_binary_http(url: &str) -> Vec<u8> {
+    let response = reqwest::blocking::get(url).expect("Failed to download file");
+    response.bytes().unwrap().to_vec()
 }
