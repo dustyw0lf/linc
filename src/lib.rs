88cf1fd9ef025b5da03864147fe1e01757e58418
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-26 12:33:53 +0300

    Make the public API clearer

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,8 @@
 
 mod elf;
 mod error;
-mod payload;
-mod techniques;
+pub mod payload;
+pub mod techniques;
 mod utils;
 
 // endregion: --- Modules
-
-// region:    --- Public API
-
-pub use payload::{Payload, PayloadResultExt, PayloadType};
-pub use techniques::{hollow, memfd};
-
-// endregion: --- Public API
