d9def536be0b18a0e5c9eec68bab199dcb58cdff
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-01 21:38:35 +0200

    Update utiliy functions to use the new Error variant names

--- src/utils.rs
+++ src/utils.rs
@@ -17,12 +17,12 @@ pub fn get_env() -> Result<Vec<CString>> {
 fn os_string_to_c_string(string: OsString) -> Result<CString> {
     let mut vector = string.into_vec();
     vector.push(0);
-    CString::from_vec_with_nul(vector).map_err(Error::FromVecWithNulError)
+    CString::from_vec_with_nul(vector).map_err(Error::FromVecWithNul)
 }
 
 pub fn str_to_vec_c_string(string: &str) -> Result<Vec<CString>> {
     string
         .split_whitespace()
-        .map(|x| CString::new(x).map_err(Error::NulError))
+        .map(|x| CString::new(x).map_err(Error::Nul))
         .collect()
 }
