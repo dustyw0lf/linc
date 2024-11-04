1fb093c6e58fd5d2f0396c5b571d775e582a46e7
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-04 23:02:02 +0200

    Update memfd_executable to support the new Payload type

--- examples/memfd_executable.rs
+++ examples/memfd_executable.rs
@@ -1,8 +1,9 @@
-use linc::payload::{Payload, PayloadResultExt, PayloadType};
+use linc::payload::{New, Payload, PayloadResultExt, PayloadType};
 use linc::techniques::memfd;
 
 fn main() {
-    let payload = Payload::from_file("/usr/bin/ls", PayloadType::Executable).set_args("-l -a -h");
+    let payload =
+        Payload::<New>::from_file("/usr/bin/ls", PayloadType::Executable).set_args("-l -a -h");
     // let payload = Payload::from_url("http://127.0.0.1:8081/ls", PayloadType::Executable).set_args("-l -a -h");
 
     // Check if the payload was created successfully
