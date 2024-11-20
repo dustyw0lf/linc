7cf772521010c34c25ef9a48f378cf1fa6b910c0
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-20 18:32:44 +0200

    feat(procfs): implement find_memory_region function

---
+++ src/primitives/procfs.rs
@@ -0,0 +1,32 @@
+use nix::unistd::Pid;
+
+use std::fs;
+
+use crate::error::Result;
+
+pub(crate) fn find_memory_region(
+    pid: Pid,
+    get_start_address: bool,
+    permissions: &str,
+) -> Result<Vec<u64>> {
+    let maps = fs::read_to_string(format!("/proc/{}/maps", pid.as_raw()))?;
+
+    let matching_lines = maps.lines().filter(|line| {
+        line.split_whitespace()
+            .nth(1)
+            .map_or(false, |perms| perms.contains(permissions))
+    });
+
+    let addrs = matching_lines
+        .map(|line| {
+            let addr_range = if get_start_address {
+                &line[..12]
+            } else {
+                &line[13..25]
+            };
+            u64::from_str_radix(addr_range, 16)
+        })
+        .collect::<Result<Vec<u64>>>()?;
+
+    Ok(addrs)
+}
